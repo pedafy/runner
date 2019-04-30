4a4c1ef6304c656e987dd60c67902c3f18dee38b
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-30 18:15:56 +0200

    Addition of script / tools for pedafy

---
+++ bin/pedafy_update_dispatch.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env sh
+
+sudo gcloud app deploy $PEDAFY_SRC/runner/dispatch.yaml
---
+++ bin/start_dev.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+
+# Add each service here
+sudo goapp serve $PEDAFY_SRC/pedafy/app.yaml $PEDAFY_SRC/pedafy-assignments/app.yaml
