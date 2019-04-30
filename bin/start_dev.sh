5176fa7a8bdf3b308b4d115d569e7de61c2f6280
Florent Poinsard <florent.poinsard@epitech.eu>
2019-04-30 18:20:54 +0200

    Modification of the start_dev script, no sudo and port 9000

--- bin/start_dev.sh
+++ bin/start_dev.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 # Add each service here
-sudo goapp serve $PEDAFY_SRC/pedafy/app.yaml $PEDAFY_SRC/pedafy-assignments/app.yaml
+goapp serve --port=9000 $PEDAFY_SRC/pedafy/app.yaml $PEDAFY_SRC/pedafy-assignments/app.yaml
