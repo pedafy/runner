3917c7b98b291cc12fe783c1ca74064deaba5aac
Florent Poinsard <florent.poinsard@epitech.eu>
2019-05-16 08:01:20 +0200

    Modification of the dev script in order to use pedafy-tasks

--- bin/start_dev.sh
+++ bin/start_dev.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 # Add each service here
-goapp serve --port=9000 $PEDAFY_SRC/pedafy/app.yaml $PEDAFY_SRC/pedafy-assignments/app.yaml
+goapp serve --port=9000 $PEDAFY_SRC_GO/pedafy/app.yaml $PEDAFY_SRC_GO/pedafy-assignments/src/app/app.yaml $PEDAFY_SRC_GO/pedafy-tasks/src/app/app.yaml
