f46f238f94d26617b73b9a244479e2ee9022ebc9
Florent Poinsard <florent.poinsard@outlook.fr>
2019-06-05 22:47:09 +0200

    Modification of the script configuration, usage of binary in the path

--- bin/start_dev.sh
+++ bin/start_dev.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
 # Add each service here
-goapp serve --port=9000 $PEDAFY_SRC_GO/pedafy/src/app/app.yaml $PEDAFY_SRC_GO/pedafy-assignments/src/app/app.yaml $PEDAFY_SRC_GO/pedafy-tasks/src/app/app.yaml
+dev_appserver.py --port=9000 $PEDAFY_SRC_GO/pedafy/src/app/app.yaml $PEDAFY_SRC_GO/pedafy-assignments/src/app/app.yaml $PEDAFY_SRC_GO/pedafy-tasks/src/app/app.yaml
